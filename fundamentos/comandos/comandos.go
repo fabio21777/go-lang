0dddfd1329f3f9992851badeb1fac2debd9b6308
fabio21777 <fabiosouza21777@gmail.com>
2024-04-01 19:52:04 -0300

    comandos

---
+++ fundamentos/comandos/comandos.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("Outro programa em  %s", "Go")
+}
+
+// principais comandos
+
+// go run comandos.go
+// go build comandos.go
+// ./comandos
+// go vet comandos.go
+// go env
+// go version
+// go help
