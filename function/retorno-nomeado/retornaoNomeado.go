a6d1a89a830621d0d86dee60956e09017f0f2522
fabio21777 <fabiosouza21777@gmail.com>
2024-04-10 20:17:10 -0300

    retorno nomeado

---
+++ function/retorno-nomeado/retornaoNomeado.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func troca(p1, p2 int) (segundo, primeiro int) {
+	segundo = p2
+	primeiro = p1
+
+	return // retorno limpo
+}
+
+func main() {
+	x, y := troca(2, 3)
+	fmt.Println(x, y)
+
+	segundo, primeiro := troca(2, 3)
+
+	fmt.Println(segundo, primeiro)
+}
