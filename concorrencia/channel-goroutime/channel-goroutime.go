c270cb0b80c2e9271411242c9833ca520ee8504a
fabio21777 <fabiosouza21777@gmail.com>
2024-04-29 20:15:34 -0300

    channel e goroutime

---
+++ concorrencia/channel-goroutime/channel-goroutime.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+// Channel (canal) - é a forma de comunicação entre goroutines
+
+func doisTresQuatroVezes(base int, c chan int) {
+	time.Sleep(time.Second)
+	c <- 2 * base // enviando dados para o canal
+
+	time.Sleep(time.Second)
+
+	c <- 3 * base // enviando dados para o canal
+
+	time.Sleep(time.Second)
+
+	c <- 4 * base // enviando dados para o canal
+}
+
+func main() {
+	c := make(chan int)
+	go doisTresQuatroVezes(2, c)
+
+	a, b := <-c, <-c // recebendo dados do canal
+
+	fmt.Println(a, b)
+
+	fmt.Println(<-c)
+}
---
+++ concorrencia/channel/channel.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	ch := make(chan int, 1)
+	ch <- 1
+	<-ch
+	ch <- 2
+
+	fmt.Println(<-ch)
+}
